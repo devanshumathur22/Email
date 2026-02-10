2ce99bd2f0a65ed6fec82a8ef3df74252a53dfca
Devanshu Mathur <devanshumathur@Devanshus-MacBook-Air.local>
2026-02-10 10:17:58 +0530

    final cors fix

--- email-sender-backend/src/main.ts
+++ email-sender-backend/src/main.ts
@@ -6,14 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
 app.enableCors({
-  origin: [
-    "http://localhost:5173",          // local frontend
-    "https://email-ap8g.vercel.app",  // vercel prod
-  ],
-  methods: "GET,POST,PATCH,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization",
+  origin: true,   // 🔥 reflect request origin automatically
+  credentials: true,
 })
-
 
 
 
