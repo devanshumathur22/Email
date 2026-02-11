44bf53e7cac07c3f625515c2aaed6b63b8197040
Devanshu Mathur <devanshumathur@Devanshus-MacBook-Air.local>
2026-02-11 22:46:32 +0530

    remove api prefix

--- email-sender-backend/src/main.ts
+++ email-sender-backend/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from "@nestjs/common"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-    app.setGlobalPrefix("api")
+    // app.setGlobalPrefix("api")
 
   // ✅ GLOBAL VALIDATION PIPE
   app.useGlobalPipes(
