bc56ee192389b9119f9b5173abfa9313f974238c
Devanshu Mathur <devanshumathur@Devanshus-MacBook-Air.local>
2026-02-11 22:32:29 +0530

    fix api base url

--- src/api.js
+++ src/api.js
@@ -1,9 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_URL
 
-if (!BASE_URL) {
-  throw new Error("VITE_API_URL is not defined")
-}
-
 export const api = async (url, options = {}) => {
   const token = localStorage.getItem("token")
 
